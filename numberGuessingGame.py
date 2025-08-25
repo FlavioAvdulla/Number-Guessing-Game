fb5a054171c004adaa660bb74576501cf2e2ed43
Flavio Avdulla <a.flavio4366@gmail.com>
2025-08-25 13:01:33 +0200

    Class button

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -22,3 +22,24 @@ HIGHLIGHT = (255, 215, 0)
 title_font = pygame.font.SysFont("arial", 48, bold=True)
 main_font = pygame.font.SysFont("arial", 32)
 message_font = pygame.font.SysFont("arial", 28)
+
+class Button:
+    def __init__(self, x, y, width, height, text):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.color = BUTTON_COLOR
+        
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect, border_radius=12)
+        pygame.draw.rect(surface, TEXT_COLOR, self.rect, 2, border_radius=12)
+        
+        text_surface = main_font.render(self.text, True, TEXT_COLOR)
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        surface.blit(text_surface, text_rect)
+        
+    def is_hover(self, pos):
+        if self.rect.collidepoint(pos):
+            self.color = BUTTON_HOVER
+            return True
+        self.color = BUTTON_COLOR
+        return False
