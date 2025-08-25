db636edfaeb7bb297c82460983d23776cdba0908
Flavio Avdulla <a.flavio4366@gmail.com>
2025-08-25 12:59:44 +0200

    Add Colors

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -9,3 +9,11 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Number Guessing Game")
+
+# Colors
+BACKGROUND = (25, 25, 40)
+TEXT_COLOR = (255, 255, 255)
+BUTTON_COLOR = (70, 130, 180)
+BUTTON_HOVER = (95, 160, 210)
+INPUT_BOX = (50, 80, 120)
+HIGHLIGHT = (255, 215, 0)
